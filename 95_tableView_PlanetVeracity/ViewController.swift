357382a83de52fc6413154a2fbe0cb833153ddf1
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-07-05 09:13:08 -0600

    Initial Commit

---
+++ 95_tableView_PlanetVeracity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  95_tableView_PlanetVeracity
+//
+//  Created by Kien Nguyen on 2021-07-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
